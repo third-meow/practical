59b4df0c1a87d59b2ad63bafa53ab0197c5695c1
The third meow <jsheaandcats@gmail.com>
2019-07-19 10:22:45 +1200

    add property class

---
+++ property.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include"property.h"
+
+Property::Property() {
+	cost = 0;
+	rent = 0;
+	house_val = 1;
+}
+
+Property::Property(int cst, int rnt) {
+	cost = cst;
+	rent = rnt;
+	house_val = 1;
+}
---
+++ property.h
@@ -0,0 +1,11 @@
+
+class Property {
+	public:
+	int cost;
+	int rent;
+	int house_val;
+	Property();
+	Property(int cst, int rnt);
+};
+
+
